92c23b41ec5ad0ca40fda49e419a39f012beb3d4
Jaume Colom <jcolomf@cirsa.com>
2018-02-23 16:54:16 +0100

    Removed test

--- app/src/test/java/com/jcolom/kotlin_arch/ExampleUnitTest.kt
+++ app/src/test/java/com/jcolom/kotlin_arch/ExampleUnitTest.kt
@@ -1,13 +1,7 @@
 package com.jcolom.kotlin_arch
 
-import com.jcolom.kotlin_arch.domain.command.main.DoCommandsMainImpl
-import com.jcolom.kotlin_arch.domain.exceptions.BaseError
-import com.jcolom.kotlin_arch.domain.command.base.PresenterCallback
-import io.reactivex.plugins.RxJavaPlugins
-import io.reactivex.schedulers.Schedulers
 import org.junit.Assert.assertEquals
 import org.junit.Test
-import org.mockito.Mock
 
 
 /**
@@ -15,28 +9,10 @@ import org.mockito.Mock
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-class ExampleUnitTest : PresenterCallback<List<String>, BaseError> {
-
-    @Mock
-    private lateinit var doCommandsMainImpl: DoCommandsMainImpl
+class ExampleUnitTest {
 
     @Test
     fun addition_isCorrect() {
-        RxJavaPlugins.setIoSchedulerHandler { Schedulers.trampoline() }
-        doCommandsMainImpl.setCallback(this)
-        doCommandsMainImpl.getListOne()
+        assertEquals(4, 2 + 2)
     }
-
-    override fun onSuccess(response: List<String>) {
-        var list = ArrayList<String>()
-        list.add("One")
-        list.add("Two")
-        list.add("Three")
-        assertEquals(list, response)
-    }
-
-    override fun onError(error: BaseError) {
-
-    }
-
 }
